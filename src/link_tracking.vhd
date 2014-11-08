ab4671e0040b465ef1054b9afc88ec25c4d093a1
Thomas Lenzi <tlenzi@ulb.ac.be>
2014-11-08 20:44:40 +0100

    8 November 2014 - 20h44

--- src/link_tracking.vhd
+++ src/link_tracking.vhd
@@ -8,31 +8,31 @@ entity link_tracking is
 port(
 
     -- Clocks and reset
-    
+
     gtp_clk_i       : in std_logic;
     vfat2_clk_i     : in std_logic;
     reset_i         : in std_logic;
-    
+
     -- GTP signals
-    
+
     rx_error_i      : in std_logic;
     rx_kchar_i      : in std_logic_vector(1 downto 0);
     rx_data_i       : in std_logic_vector(15 downto 0);
-    
+
     tx_kchar_o      : out std_logic_vector(1 downto 0);
     tx_data_o       : out std_logic_vector(15 downto 0);
-    
+
     -- IIC signals
-    
+
     vfat2_sda_i     : in std_logic_vector(1 downto 0);
     vfat2_sda_o     : out std_logic_vector(1 downto 0);
     vfat2_sda_t     : out std_logic_vector(1 downto 0);
     vfat2_scl_o     : out std_logic_vector(1 downto 0);
-    
+
     -- VFAT2 data lines
-    
+
     vfat2_dvalid_i  : in std_logic_vector(1 downto 0);
-    
+
     vfat2_data_0_i  : in std_logic;
     vfat2_data_1_i  : in std_logic;
     vfat2_data_2_i  : in std_logic;
@@ -41,43 +41,43 @@ port(
     vfat2_data_5_i  : in std_logic;
     vfat2_data_6_i  : in std_logic;
     vfat2_data_7_i  : in std_logic
-    
+
 );
 end link_tracking;
 
 architecture Behavioral of link_tracking is
-    
+
     -- VFAT2 I2C signals
-    
+
     signal vi2c_rx_en       : std_logic := '0';
     signal vi2c_rx_data     : std_logic_vector(31 downto 0) := (others => '0');
     signal vi2c_tx_ready    : std_logic := '0';
     signal vi2c_tx_done     : std_logic := '0';
     signal vi2c_tx_data     : std_logic_vector(31 downto 0) := (others => '0');
-    
+
+    -- Tracking signals
+
+    signal track_tx_ready   : std_logic := '0';
+    signal track_tx_done    : std_logic := '0';
+    signal track_tx_data    : std_logic_vector(191 downto 0) := (others => '0');
+
     -- Registers
-    
+
     signal regs_rx_en       : std_logic := '0';
     signal regs_rx_data     : std_logic_vector(47 downto 0) := (others => '0');
     signal regs_tx_ready    : std_logic := '0';
     signal regs_tx_done     : std_logic := '0';
     signal regs_tx_data     : std_logic_vector(47 downto 0) := (others => '0');
-    
+
     signal regs_write       : array128x32;
     signal regs_tri         : std_logic_vector(127 downto 0);
-    
+
     signal registers_write  : array128x32;
     signal registers_tri    : std_logic_vector(127 downto 0);
     signal registers_read   : array128x32;
-    
-    -- Tracking signals
-    
-    signal track_tx_ready   : std_logic := '0';
-    signal track_tx_done    : std_logic := '0';
-    signal track_tx_data    : std_logic_vector(191 downto 0) := (others => '0');
-    
+
     -- Counters
-    
+
     signal rx_error_counter : std_logic_vector(31 downto 0) := (others => '0');
     signal vi2c_rx_counter  : std_logic_vector(31 downto 0) := (others => '0');
     signal vi2c_tx_counter  : std_logic_vector(31 downto 0) := (others => '0');
@@ -85,9 +85,9 @@ architecture Behavioral of link_tracking is
     signal regs_tx_counter  : std_logic_vector(31 downto 0) := (others => '0');
 
     -- ChipScope signals
-    
+
     signal tx_data          : std_logic_vector(15 downto 0);
-    
+
     signal cs_icon0         : std_logic_vector(35 downto 0);
     signal cs_icon1         : std_logic_vector(35 downto 0);
     signal cs_in            : std_logic_vector(31 downto 0);
@@ -100,7 +100,7 @@ begin
     --================================--
     -- GTP
     --================================--
-    
+
     gtp_rx_mux_inst : entity work.gtp_rx_mux
     port map(
         gtp_clk_i   => gtp_clk_i,
@@ -112,26 +112,26 @@ begin
         rx_kchar_i  => rx_kchar_i,
         rx_data_i   => rx_data_i
     );
-    
+
     gtp_tx_mux_inst : entity work.gtp_tx_mux
     port map(
         gtp_clk_i       => gtp_clk_i,
         reset_i         => reset_i,
         vi2c_ready_i    => vi2c_tx_ready,
         vi2c_done_o     => vi2c_tx_done,
-        vi2c_data_i     => vi2c_tx_data, 
+        vi2c_data_i     => vi2c_tx_data,
         regs_ready_i    => regs_tx_ready,
         regs_done_o     => regs_tx_done,
-        regs_data_i     => regs_tx_data, 
+        regs_data_i     => regs_tx_data,
         track_ready_i   => track_tx_ready,
         track_done_o    => track_tx_done,
-        track_data_i    => track_tx_data, 
+        track_data_i    => track_tx_data,
         tx_kchar_o      => tx_kchar_o,
         tx_data_o       => tx_data
     );
-    
+
     tx_data_o <= tx_data;
-    
+
     --================================--
     -- VFAT2 I2C
     --================================--
@@ -150,12 +150,35 @@ begin
         sda_t           => vfat2_sda_t,
         scl_o           => vfat2_scl_o
     );
-    
+
+    --================================--
+    -- Tracking path
+    --================================--
+
+    tracking_core_inst : entity work.tracking_core
+    port map(
+        gtp_clk_i       => gtp_clk_i,
+        vfat2_clk_i     => vfat2_clk_i,
+        reset_i         => reset_i,
+        tx_ready_o      => track_tx_ready,
+        tx_done_i       => track_tx_done,
+        tx_data_o       => track_tx_data,
+        vfat2_dvalid_i  => vfat2_dvalid_i,
+        vfat2_data_0_i  => vfat2_data_0_i,
+        vfat2_data_1_i  => vfat2_data_1_i,
+        vfat2_data_2_i  => vfat2_data_2_i,
+        vfat2_data_3_i  => vfat2_data_3_i,
+        vfat2_data_4_i  => vfat2_data_4_i,
+        vfat2_data_5_i  => vfat2_data_5_i,
+        vfat2_data_6_i  => vfat2_data_6_i,
+        vfat2_data_7_i  => vfat2_data_7_i
+    );
+
     --================================--
     -- Registers & mapping
     --================================--
 
-    registers_core_inst : entity work.registers_core 
+    registers_core_inst : entity work.registers_core
     port map(
         fabric_clk_i    => gtp_clk_i,
         reset_i         => reset_i,
@@ -177,63 +200,40 @@ begin
         wbus_t          => registers_tri,
         rbus_o          => registers_read
     );
-    
+
     registers_tri(127 downto 123) <= (others => '1');
     registers_write(127) <= vi2c_rx_counter;
     registers_write(126) <= vi2c_tx_counter;
     registers_write(125) <= regs_rx_counter;
     registers_write(124) <= regs_tx_counter;
     registers_write(123) <= rx_error_counter;
-    
-    registers_tri(122 downto 0) <= regs_tri(122 downto 0); 
-    registers_write(122 downto 0) <= regs_write(122 downto 0); 
-    
+
+    registers_tri(122 downto 0) <= regs_tri(122 downto 0);
+    registers_write(122 downto 0) <= regs_write(122 downto 0);
+
     --================================--
-    -- Registers
-    --================================--    
-    
+    -- Counters
+    --================================--
+
     rx_error_counter_inst : entity work.counter port map(fabric_clk_i => gtp_clk_i, reset_i => reset_i, en_i => rx_error_i, data_o => rx_error_counter);
-    
+
     vi2c_rx_counter_inst : entity work.counter port map(fabric_clk_i => gtp_clk_i, reset_i => reset_i, en_i => vi2c_rx_en, data_o => vi2c_rx_counter);
     vi2c_tx_counter_inst : entity work.counter port map(fabric_clk_i => gtp_clk_i, reset_i => reset_i, en_i => vi2c_tx_done, data_o => vi2c_tx_counter);
-    
+
     regs_rx_counter_inst : entity work.counter port map(fabric_clk_i => gtp_clk_i, reset_i => reset_i, en_i => regs_rx_en, data_o => regs_rx_counter);
     regs_tx_counter_inst : entity work.counter port map(fabric_clk_i => gtp_clk_i, reset_i => reset_i, en_i => regs_tx_done, data_o => regs_tx_counter);
 
     --================================--
-    -- Tracking path
-    --================================--  
-
-    tracking_core_inst : entity work.tracking_core
-    port map(
-        gtp_clk_i       => gtp_clk_i,
-        vfat2_clk_i     => vfat2_clk_i,
-        reset_i         => reset_i,
-        tx_ready_o      => track_tx_ready,
-        tx_done_i       => track_tx_done,
-        tx_data_o       => track_tx_data,
-        vfat2_dvalid_i  => vfat2_dvalid_i,
-        vfat2_data_0_i  => vfat2_data_0_i,
-        vfat2_data_1_i  => vfat2_data_1_i,
-        vfat2_data_2_i  => vfat2_data_2_i,
-        vfat2_data_3_i  => vfat2_data_3_i,
-        vfat2_data_4_i  => vfat2_data_4_i,
-        vfat2_data_5_i  => vfat2_data_5_i,
-        vfat2_data_6_i  => vfat2_data_6_i,
-        vfat2_data_7_i  => vfat2_data_7_i
-    );
-    
-    --================================--
     -- ChipScope
     --================================--
-    
+
     chipscope_icon_inst : entity work.chipscope_icon port map (CONTROL0 => cs_icon0, CONTROL1 => cs_icon1);
-    
+
     chipscope_vio_inst : entity work.chipscope_vio port map (CONTROL => cs_icon0, ASYNC_IN => cs_in, ASYNC_OUT => cs_out);
-    
+
     chipscope_ila_inst : entity work.chipscope_ila port map (CONTROL => cs_icon1, CLK => gtp_clk_i, TRIG0 => cs_ila0, TRIG1 => cs_ila1);
-    
+
     cs_ila0 <= tx_data & rx_data_i;
     cs_ila1 <= x"0000" & x"000" & track_tx_done & track_tx_ready & vfat2_data_0_i & vfat2_dvalid_i(0);
-    
+
 end Behavioral;
